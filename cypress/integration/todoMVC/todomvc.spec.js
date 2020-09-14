98b4754eb4d2daee5bbfaf3997a2633e2eeeff31
siddharth <tech.sid17@gmail.com>
2020-09-14 10:40:48 +0530

    demo fail

--- cypress/integration/todoMVC/todomvc.spec.js
+++ cypress/integration/todoMVC/todomvc.spec.js
@@ -1,13 +1,11 @@
 /// <reference types="cypress" />
 
-
-
 it('should be able to add a new todo to the list', () => {
     cy.visit('https://todomvc-app-for-testing.surge.sh');
     debugger;
     cy.get('.new-todo', {timeout: 6000}).type('wash utensils {enter}')
     debugger;
-    cy.get('label').should('have.text', 'wash utensils').debug();
+    cy.get('label').should('have.text', 'wash utenils').debug();
     cy.get('.toggle').should('not.be.checked')
     cy.get('.toggle').click()
     cy.get('label').should('have.css', 'text-decoration-line', 'line-through');
